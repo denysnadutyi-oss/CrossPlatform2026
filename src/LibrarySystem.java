e01ced5304a1cd88a3df140a1f54a078c3f78aed
denysnadutyi-oss <denys.nadutyi@student.karazina.ua>
2026-05-19 16:00:38 +0300

    Лаб 2 версія 3

--- src/LibrarySystem.java
+++ src/LibrarySystem.java
@@ -1,16 +1,18 @@
 import java.io.*;
 import java.util.*;
 
-class Author { // НЕ Serializable
+class Author implements Externalizable {
     private String name;
     public Author() {}
     public Author(String name) { this.name = name; }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+    @Override public void writeExternal(ObjectOutput out) throws IOException { out.writeUTF(name); }
+    @Override public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException { name = in.readUTF(); }
     @Override public String toString() { return "Author{name='" + name + "'}"; }
 }
 
-class Book { // НЕ Serializable
+class Book implements Externalizable {
     private String title;
     private Author author;
     public Book() {}
@@ -19,78 +21,50 @@ class Book { // НЕ Serializable
     public void setTitle(String title) { this.title = title; }
     public Author getAuthor() { return author; }
     public void setAuthor(Author author) { this.author = author; }
+    @Override public void writeExternal(ObjectOutput out) throws IOException { out.writeUTF(title); out.writeObject(author); }
+    @Override public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException { title = in.readUTF(); author = (Author) in.readObject(); }
     @Override public String toString() { return "Book{title='" + title + "', author=" + author + "}"; }
 }
 
-class Reader { // НЕ Serializable
+class Bookshelf implements Externalizable {
+    private List<Book> books = new ArrayList<>();
+    public Bookshelf() {}
+    public List<Book> getBooks() { return books; }
+    public void setBooks(List<Book> books) { this.books = books; }
+    public void addBook(Book book) { books.add(book); }
+    @Override public void writeExternal(ObjectOutput out) throws IOException { out.writeObject(books); }
+    @Override public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException { books = (List<Book>) in.readObject(); }
+    @Override public String toString() { return "Bookshelf{" + books + "}"; }
+}
+
+class Reader implements Externalizable {
     private String name;
     public Reader() {}
     public Reader(String name) { this.name = name; }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+    @Override public void writeExternal(ObjectOutput out) throws IOException { out.writeUTF(name); }
+    @Override public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException { name = in.readUTF(); }
     @Override public String toString() { return "Reader{name='" + name + "'}"; }
 }
 
-class Bookshelf implements Serializable {
-    private transient List<Book> books = new ArrayList<>(); // transient
-    public Bookshelf() {}
-    public List<Book> getBooks() { return books; }
-    public void setBooks(List<Book> books) { this.books = books; }
-    public void addBook(Book book) { books.add(book); }
-    @Override public String toString() { return "Bookshelf{" + books + "}"; }
-
-    private void writeObject(ObjectOutputStream oos) throws IOException {
-        oos.defaultWriteObject();
-        oos.writeInt(books.size());
-        for (Book b : books) {
-            oos.writeUTF(b.getTitle());
-            oos.writeUTF(b.getAuthor().getName());
-        }
-    }
-
-    private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
-        ois.defaultReadObject();
-        books = new ArrayList<>();
-        int size = ois.readInt();
-        for (int i = 0; i < size; i++) {
-            String title = ois.readUTF();
-            String authorName = ois.readUTF();
-            books.add(new Book(title, new Author(authorName)));
-        }
-    }
-}
-
-class Rent implements Serializable {
-    private transient Book book; // transient
-    private transient Reader reader; // transient
+class Rent implements Externalizable {
+    private Book book;
+    private Reader reader;
     public Rent() {}
     public Rent(Book book, Reader reader) { this.book = book; this.reader = reader; }
     public Book getBook() { return book; }
     public void setBook(Book book) { this.book = book; }
     public Reader getReader() { return reader; }
     public void setReader(Reader reader) { this.reader = reader; }
+    @Override public void writeExternal(ObjectOutput out) throws IOException { out.writeObject(book); out.writeObject(reader); }
+    @Override public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException { book = (Book) in.readObject(); reader = (Reader) in.readObject(); }
     @Override public String toString() { return "Rent{book=" + book + ", reader=" + reader + "}"; }
-
-    private void writeObject(ObjectOutputStream oos) throws IOException {
-        oos.defaultWriteObject();
-        oos.writeUTF(book.getTitle());
-        oos.writeUTF(book.getAuthor().getName());
-        oos.writeUTF(reader.getName());
-    }
-
-    private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
-        ois.defaultReadObject();
-        String title = ois.readUTF();
-        String authorName = ois.readUTF();
-        book = new Book(title, new Author(authorName));
-        String readerName = ois.readUTF();
-        reader = new Reader(readerName);
-    }
 }
 
-class Library implements Serializable {
+class Library implements Externalizable {
     private Bookshelf bookshelf = new Bookshelf();
-    private transient List<Reader> readers = new ArrayList<>(); // transient
+    private List<Reader> readers = new ArrayList<>();
     private List<Rent> rents = new ArrayList<>();
     public Library() {}
     public Bookshelf getBookshelf() { return bookshelf; }
@@ -101,23 +75,7 @@ class Library implements Serializable {
     public void setRents(List<Rent> rents) { this.rents = rents; }
     public void addReader(Reader reader) { readers.add(reader); }
     public void rentBook(Book book, Reader reader) { rents.add(new Rent(book, reader)); }
+    @Override public void writeExternal(ObjectOutput out) throws IOException { out.writeObject(bookshelf); out.writeObject(readers); out.writeObject(rents); }
+    @Override public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException { bookshelf = (Bookshelf) in.readObject(); readers = (List<Reader>) in.readObject(); rents = (List<Rent>) in.readObject(); }
     @Override public String toString() { return "Library{" + bookshelf + ", " + readers + ", " + rents + "}"; }
-
-    private void writeObject(ObjectOutputStream oos) throws IOException {
-        oos.defaultWriteObject();
-        oos.writeInt(readers.size());
-        for (Reader r : readers) {
-            oos.writeUTF(r.getName());
-        }
-    }
-
-    private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
-        ois.defaultReadObject();
-        readers = new ArrayList<>();
-        int size = ois.readInt();
-        for (int i = 0; i < size; i++) {
-            String name = ois.readUTF();
-            readers.add(new Reader(name));
-        }
-    }
 }
