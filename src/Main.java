0973008b93a3e0a8322f071a14be4fdabf3e43e3
denysnadutyi-oss <denys.nadutyi@student.karazina.ua>
2026-05-19 11:52:00 +0300

    Завдання 1(графічне)

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,7 @@
-import java.lang.reflect.Proxy;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        Calculator calculator =
-                new CalculatorImpl();
-
-        Calculator proxyCalculator =
-                (Calculator) Proxy.newProxyInstance(
-
-                        Calculator.class.getClassLoader(),
-
-                        new Class[] { Calculator.class },
-
-                        new MethodProfilerHandler(
-                                calculator
-                        )
-                );
-
-        int sum =
-                proxyCalculator.add(5, 3);
-
-        int multiply =
-                proxyCalculator.multiply(4, 6);
-
-        double divide =
-                proxyCalculator.divide(10, 2);
-
-        System.out.println("\nFINAL RESULTS:");
-
-        System.out.println("Sum = " + sum);
-
-        System.out.println(
-                "Multiply = " + multiply
-        );
-
-        System.out.println(
-                "Divide = " + divide
-        );
+        new AnalyzerFrame();
     }
 }
